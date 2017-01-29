4292b280edcf64d97a02a45b774a853fa0bbbae0
zxc0328 <120797887@qq.com>
2017-01-29 12:34:37 +0800

    finish 46

--- permutations.js
+++ permutations.js
@@ -1,9 +1,29 @@
-function subsets(s) {
-        if (s.length === 0){
-            return []
-        }else {
-            let rest = subsets(s.splice(1))
-            return rest.concat(rest.map((x) => { return [s[0],...x];}))
-        }
-}
-subsets([1,2])
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    function Permutations(arr){
+        let container = []
+        container = arr.reduce((result, val, index) => {
+            let rest = Permutations(immutableDelete(arr, index))
+            if (rest.length === 0) {
+                rest = Array.from(new Array(1))
+            }
+            return result.concat(rest.map((x) => {
+                if (!x) {
+                    return [arr[index]]
+                }else {
+                    return [arr[index],...x]
+                }
+            }))
+        }, [])
+        return container
+    }
+
+    function immutableDelete (arr, index) {
+        return arr.slice(0,index).concat(arr.slice(index+1))
+    }
+    
+    return Permutations(nums)
+};
